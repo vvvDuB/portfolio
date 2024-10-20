644b4f5e3b766c0d3a951e8e3208b5d6997cb8ac
vvvDuB <atefjmai99@gmail.com>
2024-10-20 22:42:24 +0200

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-[#272932]">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
