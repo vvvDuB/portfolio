c14d607ee03f5e95cedae94b98c761f3977178d9
vvvDuB <atefjmai99@gmail.com>
2024-10-20 23:36:45 +0200

    working on header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Header from "./Header";
 
 function App() {
+  let currentLabel = "";
+
   return (
-    <div className="w-screen h-screen bg-[#272932]">
+    <div className="w-screen h-screen bg-[#272932] px-96 py-16 font-custom">
       <Header></Header>
+      <h1>{currentLabel}</h1>
     </div>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+
 function Header() {
-  return <h1>Header</h1>;
+  const labels = ["whoami", "socials"];
+  const [currentLabel, setCurrentLabel] = useState("");
+
+  function currentLabelHandler(label) {
+    setCurrentLabel(label);
+  }
+
+  return (
+    <header className="flex flex-col space-y-8 border-0 border-red-600">
+      <h1 className="text-[#FDF500] text-5xl">./ 0xDuB</h1>
+      <div className="flex space-x-16 border-b-2 border-[#FDF500]">
+        {labels.map((label) => (
+          <Labels
+            key={label}
+            label={label}
+            currentLabel={currentLabel}
+            currentLabelHandler={currentLabelHandler}
+          />
+        ))}
+      </div>
+      <h1>{currentLabel}</h1>
+    </header>
+  );
+}
+
+function Labels({ label, currentLabel, currentLabelHandler }) {
+  return (
+    <h1
+      onClick={() => currentLabelHandler(label)}
+      className={
+        currentLabel === label
+          ? "text-[#FDF500] cursor-pointer hover:text-[#FDF500] text-3xl my-8"
+          : "text-[#D1C5C0] cursor-pointer hover:text-[#FDF500] text-3xl my-8"
+      }
+    >
+      {label}
+    </h1>
+  );
 }
 
 export default Header;
