03dffb630418712e653d302d68be1b52b6f768a2
vvvDuB <atefjmai99@gmail.com>
2024-10-24 01:45:34 +0200

    Working on 2023 section

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,6 +1,12 @@
 import PTLogo from "./assets/images/PenTest+ce certified Logo.png";
 import CCNALogo from "./assets/images/CCNA.png";
 import LinuxLogo from "./assets/images/Linux.png";
+import React from "./assets/images/React.png";
+import Angular from "./assets/images/Angular.png";
+import Typescript from "./assets/images/Typescript.png";
+import CSS from "./assets/images/CSS.png";
+import JS from "./assets/images/JS.png";
+import HTML from "./assets/images/HTML.png";
 import { useEffect, useRef, useState } from "react";
 import gsap from "gsap"; // Assicurati di importare gsap
 import TypingEffect from "./util/TypingEffect";
@@ -8,43 +14,88 @@ import TypingEffect from "./util/TypingEffect";
 function Body() {
   const [counter, setCounter] = useState(0);
 
-  const img = <div className="w-full grid place-items-end"><img className="w-80 mt-12" src="https://tryhackme-badges.s3.amazonaws.com/0xDuB.png" alt="Your Image Badge" /></div>
+  const img = (
+    <div className="w-full grid place-items-end">
+      <img
+        className="w-80 mt-12"
+        src="https://tryhackme-badges.s3.amazonaws.com/0xDuB.png"
+        alt="Your Image Badge"
+      />
+    </div>
+  );
 
-  const first = {
+  const cert1 = {
     date: 2024,
     title: "Junior Penetration Tester",
     certs: [
       {
         logo: PTLogo,
         certTitle: "CompTIA PenTest+ ce Certification",
-        certText: "Expires on September 25, 2027"
-      }
+        certText: "Expires on September 25, 2027",
+      },
     ],
-    img: img
-  }
+    img: img,
+  };
 
-  const second = {
+  const cert2 = {
     date: 2024,
     title: "System Administrator",
     certs: [
       {
         logo: CCNALogo,
         certTitle: "Cisco CCNAv7: Switching, Routing, and Wireless Essentials",
-        certText: "24/07/2024"
+        certText: "24/07/2024",
       },
       {
         logo: LinuxLogo,
         certTitle: "NDG Linux Essentials",
-        certText: "16/05/2024"
-      }
+        certText: "16/05/2024",
+      },
     ],
-  }
+  };
 
-  const pages = [first, second];
+  const cert3 = {
+    date: 2023,
+    title: "Web Developer",
+    certs: [
+      {
+        logo: React,
+        certTitle: "React",
+        certText: "",
+      },
+      {
+        logo: Angular,
+        certTitle: "Angular",
+        certText: "",
+      },
+      {
+        logo: Typescript,
+        certTitle: "Typescript",
+        certText: "",
+      },
+      {
+        logo: CSS,
+        certTitle: "CSS",
+        certText: "",
+      },
+      {
+        logo: JS,
+        certTitle: "JS",
+        certText: "",
+      },
+      {
+        logo: HTML,
+        certTitle: "HTML",
+        certText: "",
+      },
+    ],
+  };
+
+  const pages = [cert1, cert2, cert3];
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCounter(prevCounter => (prevCounter === 0 ? 1 : 0));
+      setCounter((prevCounter) => (prevCounter + 1) % 3);
     }, 5000);
 
     return () => clearInterval(intervalId);
@@ -57,7 +108,13 @@ function Body() {
       </div>
       <TypingEffect text={pages[counter].title} />
       {pages[counter].certs.map((cert) => (
-        <Certs key={cert.certTitle} logo={cert.logo} title={cert.certTitle} text={cert.certText} counter={counter}/>
+        <Certs
+          key={cert.certTitle}
+          logo={cert.logo}
+          title={cert.certTitle}
+          text={cert.certText}
+          counter={counter}
+        />
       ))}
       {pages[counter].img && pages[0].img}
     </div>
@@ -69,11 +126,11 @@ function Certs({ logo, title, text, counter }) {
 
   useEffect(() => {
     gsap.fromTo(
-      elementRef.current, 
+      elementRef.current,
       { opacity: 0 },
       { opacity: 1, duration: 2 }
     );
-  }, [])
+  }, []);
 
   useEffect(() => {
     gsap.to(elementRef.current, { opacity: 0, duration: 2 }).then(() => {
