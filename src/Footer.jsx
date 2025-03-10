201593f3864dcac22dfa1902f4b6cc9cf71f16fc
vvvDuB <atefjmai99@gmail.com>
2025-03-10 17:56:09 +0100

    Mobile fix: socials -> footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer({ isMobile, currentLabel }) {
   return (
-      <div className={`text-customWhite-50 text-xs flex border-t-2 border-customPrimary-50 py-4 ${isMobile ? "justify-center" : "mt-12 justify-between"} ${currentLabel === "socials" ? "mt-[5.25rem]" : "mt-24"}`}>
+      <div className={`text-customWhite-50 text-xs flex border-t-2 border-customPrimary-50 py-4 ${isMobile ? "justify-center" : "mt-12 justify-between"} ${currentLabel === "socials" ? "mt-20" : "mt-24"}`}>
         <p className={`${isMobile && "text-center"}`}>
           Designed and developed by{" "}
           <a
