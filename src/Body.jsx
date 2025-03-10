e4ae567d9ba06e6f1b7e201381a25b2913137300
vvvDuB <atefjmai99@gmail.com>
2025-03-10 18:05:46 +0100

    Mobile fix: whoami -> sysadmin

--- src/Body.jsx
+++ src/Body.jsx
@@ -36,7 +36,7 @@ function Body({ isMobile }) {
   const cert1 = {
     date: 2024,
     title: "Junior Penetration Tester",
-    classNames: ["cursor-pointer flex items-center space-x-4 xl:my-24 my-16", "w-16", ""],
+    classNames: [`cursor-pointer flex items-center space-x-4 xl:my-24 ${isMobile ? "my-8" : "my-16"}`, "w-16", ""],
     certs: [
       {
         logo: PTLogo,
@@ -50,7 +50,7 @@ function Body({ isMobile }) {
   const cert2 = {
     date: 2024,
     title: "System Administrator",
-    classNames: ["cursor-pointer flex items-center space-x-4 xl:my-24 my-12", "w-16" , ""],
+    classNames: [`cursor-pointer flex items-center space-x-4 xl:my-24 ${isMobile ? "my-8" : "my-12"}`, "w-16" , ""],
     certs: [
       {
         logo: CCNALogo,
@@ -68,7 +68,7 @@ function Body({ isMobile }) {
   const cert3 = {
     date: 2023,
     title: "Web Developer",
-    classNames: [`cursor-pointer flex items-center space-x-4 xl:my-16 ${!isMobile && "my-10"} my-1`, "w-12", `grid ${isMobile ? "grid-cols-2 gap-2" : "grid-cols-3 gap-2"} mx-4`],
+    classNames: [`cursor-pointer flex items-center space-x-4 xl:my-16 ${isMobile ? "my-4" : "my-10"}`, "w-12", `grid ${isMobile ? "grid-cols-2 gap-2" : "grid-cols-3 gap-2"} mx-4`],
     certs: [
       {
         logo: React,
