9a98ac3b88cc8bf429b6d133aa2de6c34f495453
vvvDuB <atefjmai99@gmail.com>
2025-03-10 18:28:46 +0100

    Clean up & add metadata

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer({ isMobile, currentLabel }) {
   return (
-      <div className={`text-customWhite-50 text-xs flex border-t-2 border-customPrimary-50 py-4 ${isMobile ? "justify-center" : "mt-12 justify-between"} mt-20"`}>
+      <div className={`text-customWhite-50 text-xs flex border-t-2 border-customPrimary-50 py-4 ${isMobile ? "justify-center" : "mt-12 justify-between"} mt-20`}>
         <p className={`${isMobile && "text-center"}`}>
           Designed and developed by{" "}
           <a
