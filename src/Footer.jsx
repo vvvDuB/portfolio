1591356047b210b947f590bf07471c28165d2ecd
unknown <atefjma99@gmail.com>
2025-04-11 20:03:21 +0200

    Clean up some codez

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,4 +1,4 @@
-function Footer({ isMobile, currentLabel }) {
+function Footer({ isMobile }) {
   return (
       <div className={`text-customWhite-50 text-xs flex border-t-2 border-customPrimary-50 py-4 ${isMobile ? "justify-center" : "mt-32 justify-between"} mt-20`}>
         <p className={`${isMobile && "text-center"}`}>
